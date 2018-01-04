51ba361cf20210a46cda30aae024f0509d2fd147
Anton Zylyov <anton@sebekon.ru>
2018-01-04 20:35:24 +0300

    Проверка почты с целью поиска файлов с отчетами сбербанка

--- src/Controller/CheckEmail.php
+++ src/Controller/CheckEmail.php
@@ -6,6 +6,7 @@
 namespace MoneyStat\Controller;
 
 
+use MoneyStat\Parser\Sberbank;
 use Psr\Container\ContainerInterface;
 
 class CheckEmail
@@ -19,7 +20,36 @@ class CheckEmail
 
 	public function __invoke($request, $response, $args)
 	{
-		// @todo checking email
+		$settings = $this->container['settings']['email'];
+		$mailbox = new \PhpImap\Mailbox(
+			$settings['host'],
+			$settings['user'],
+			$settings['password'],
+			$settings['attachments_dir']
+		);
+		$mailsIds = $mailbox->searchMailbox('UNSEEN');
+		if (!$mailsIds) {
+			$this->container->logger->debug('Unread messages not found in mailbox');
+		} else {
+			foreach ($mailsIds as $mailId) {
+				$mail = $mailbox->getMail($mailId);
+				$mailbox->markMailAsRead($mailId);
+				if (strpos($mail->subject, 'Statement report') !== false) {
+					$attachments = $mail->getAttachments();
+					if (!empty($attachments)) {
+						foreach ($attachments as $attachment) {
+							if (substr($attachment->name, -4) == '.txt') {
+								$this->container->logger->info('Found new attachment for parse: ' . $attachment->filePath);
+								$parser = new Sberbank();
+								$parser->parse($attachment->filePath);
+							}
+						}
+					}
+				}
+			}
+		}
+
+
 		return $response;
 	}
 }
---
+++ src/Parser/Sberbank.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by mr.Umnik.
+ */
+
+namespace MoneyStat\Parser;
+
+
+class Sberbank
+{
+	public function parse($filename)
+	{
+		//@todo parse statement report
+	}
+}
